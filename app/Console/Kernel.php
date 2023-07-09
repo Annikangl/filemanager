c9470b7781aea9a30ddbc00c3bd559f2a852a723
Ivan <svinpauk78@gmail.com>
2023-07-09 18:40:49 +0300

    Add cron container, change Add DeleteExpiredUploads.php, add to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('uploads:delete-expired')->daily();
+        $schedule->command('uploads:delete-expired')->everyMinute();
     }
 
     protected function commands(): void
